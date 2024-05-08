37ae1cde88ad06a4a6a5a50e5434cb0d78f45199
Arseniy PSV <arsypsv77@gmail.com>
2024-05-08 18:43:36 +0300

    add integer project

---
+++ Integer/Integer.cpp
@@ -0,0 +1,35 @@
+#include "Integer.h"
+
+Integer Integer::add(Integer a) {
+	return Integer{ value + a.getValue() };
+};
+
+Integer Integer::sub(Integer a) {
+	return Integer{ value - a.getValue() };
+	
+};
+
+Integer Integer::mul(Integer a) {
+	return Integer{ value * a.getValue() };
+};
+
+Integer Integer::div(Integer a) {
+	return Integer{ value / a.getValue() };
+};
+
+Integer Integer::mod(Integer a) {
+	return Integer{ value % a.getValue() };
+};
+
+Integer Integer::pow(int n) {
+	int number = 1;
+	for (int i = 0; i < n; i++)
+	{
+		number *= value;
+	}
+	return Integer(number);
+};
+
+Integer Integer::opp() {
+	return Integer(-value);
+};
---
+++ Integer/Integer.h
@@ -0,0 +1,20 @@
+#pragma once
+class Integer
+{
+public:
+	int value;
+private:
+	Integer() :value(0) {}
+	Integer(int value) :value(value) {}
+
+	Integer add(Integer a);
+	Integer sub(Integer a);
+	Integer mul(Integer a);
+	Integer div(Integer a);
+	Integer mod(Integer a);
+	Integer pow(int n);
+	Integer opp();
+
+	int getValue();
+	void setValue(int value);
+};
