74369128c3e78a14d5e3be1d6b0bac9131ae7f3f
Arseniy PSV <arsypsv77@gmail.com>
2024-05-27 18:48:11 +0300

    refactoring

--- GraphicsProject/main.cpp
+++ GraphicsProject/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
 	Rectangle* rect = new Rectangle(4, 5, 1, 1);
 	Circle* circle = new Circle(5, 3, 4);
-	
+	 
 	Editor editor;
 
 	cout << editor.justDoIt(rect) << endl;
