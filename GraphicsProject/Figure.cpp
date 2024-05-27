776050ef7eef93ff6aace6090f1dba778cb2fa8b
Arseniy PSV <arsypsv77@gmail.com>
2024-05-27 18:47:57 +0300

    refactoring

--- GraphicsProject/Figure.cpp
+++ GraphicsProject/Figure.cpp
@@ -1,1 +1,22 @@
 #include "Figure.h"
+
+int Figure::getX() {
+	return x;
+}
+
+int Figure::getY() {
+	return y;
+}
+
+void Figure::getX(int x) {
+	this->x = x;
+}
+
+void Figure::getY(int y) {
+	this->y = y;
+}
+
+string Figure::draw() {
+	return "start point - x = "
+		+ to_string(x) + ", y = " + to_string(y);
+}
